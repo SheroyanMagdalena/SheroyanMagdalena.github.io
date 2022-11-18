37d66088e0a7be96cfae89fdb808483cf4424747
SheroyanMagdalena <117111650+SheroyanMagdalena@users.noreply.github.com>
2022-11-18 23:02:26 +0400

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,4 +1,7 @@
-/*let a, b;
+
+/*alert("1. sum, 2.difference, 3. product, 4. quotient, 5. remainder  ")
+
+let a, b;
 a = prompt("input number a = ");
 b = prompt("input number b = ");
 
@@ -9,7 +12,9 @@ alert(+a / +b);
 alert(+a % +b);*/
 
 
-/*let namee, age;
+/*
+alert("Print  your name and age in the following format: “My name is ____, I am ____ .” Name and age should be variables with defined values.");
+let namee, age;
 namee = prompt("What is your name?");
 age = prompt("how old are you?");
 if(isNaN(age)){
@@ -18,25 +23,34 @@ if(isNaN(age)){
 else{
     alert(`My name is ${namee}, I am ${age} years old`);
 
-}*/
+}
+*/
 
-/*let num = prompt("Enter a number");
+/*
+alert("3.Check whether a given number is negative. Print “yes”, if it is negative, print “no” otherwise.");
+let num = prompt("Enter a number");
 if(num <0 ){
 alert("YES");
 }
  else{
     alert("NO");
- }*/
+ }
+*/
 
- /*let num = prompt("Enter a number");
+/*
+ alert("Given a number. Print “odd” if the number is odd and “even” if it’s even.");
+ let num = prompt("Enter a number");
  if(num % 2 == 0 ){
  alert("even");
  }
   else{
      alert("odd");
-  }*/
+  }
+*/
 
- /* let num1, num2;
+/*
+ alert("Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.");
+ let num1, num2;
   num1 = prompt("Enter a number");
   num2 = prompt("Enter a number");
 
